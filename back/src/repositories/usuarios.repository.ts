950168f6aba872d15e4b499912fe9fddae76f3f7
PHansLM <pablito.hans@hotmail.com>
2025-10-02 23:30:38 -0400

    fix ocultacion de informacion sensible

--- back/src/repositories/usuarios.repository.ts
+++ back/src/repositories/usuarios.repository.ts
@@ -3,30 +3,86 @@ import { UsuarioCreate, UsuarioUpdate } from '../types/usuarios.types';
 
 
 export const getAllUsuarios = async () => {
-  return prisma.usuario.findMany();
+  return prisma.usuario.findMany({
+    select: {
+      id: true,
+      email: true,
+      nombre: true,
+      apellido: true,
+      avatar_url: true,
+      bio: true,
+      provider: true,
+      fecha_registro: true,
+      ultimo_acceso: true
+    }
+  });
 };
 
 export const getUsuarioById = async (id: number) => {
   return prisma.usuario.findUnique({
-    where: { id }
+    where: { id },
+    select: {
+      id: true,
+      email: true,
+      nombre: true,
+      apellido: true,
+      avatar_url: true,
+      bio: true,
+      provider: true,
+      fecha_registro: true,
+      ultimo_acceso: true
+    }
   });
 };
 
 export const createUsuario = async (data: UsuarioCreate) => {
   return prisma.usuario.create({
-    data
+    data,
+    select: {
+      id: true,
+      email: true,
+      nombre: true,
+      apellido: true,
+      avatar_url: true,
+      bio: true,
+      provider: true,
+      fecha_registro: true,
+      ultimo_acceso: true
+    }
   });
 };
 
 export const updateUsuario = async (id: number, data: UsuarioUpdate) => {
   return prisma.usuario.update({
     where: { id },
-    data
+    data,
+    select: {
+      id: true,
+      email: true,
+      nombre: true,
+      apellido: true,
+      avatar_url: true,
+      bio: true,
+      provider: true,
+      fecha_registro: true,
+      ultimo_acceso: true
+    }
   });
 };
 
 export const deleteUsuario = async (id: number) => {
   return prisma.usuario.delete({
-    where: { id }
+    where: { id },
+    select: {
+      id: true,
+      email: true,
+      nombre: true,
+      apellido: true,
+      avatar_url: true,
+      bio: true,
+      provider: true,
+      fecha_registro: true,
+      ultimo_acceso: true
+    }
   });
 };
