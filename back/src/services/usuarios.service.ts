3e71dfbe1aa5512b3c22c70bd6fec2c4784f1e70
PHansLM <pablito.hans@hotmail.com>
2025-10-06 11:07:09 -0400

    feat(register usuario): email and password validations

--- back/src/services/usuarios.service.ts
+++ back/src/services/usuarios.service.ts
@@ -1,5 +1,6 @@
 import * as userRepo from '../repositories/usuarios.repository';
 import { UsuarioCreate, UsuarioUpdate } from '../types/usuarios.types';
+import bcrypt from 'bcrypt';
 
 export const getUsuarios = () => {
   return userRepo.getAllUsuarios();
@@ -9,8 +10,30 @@ export const getUsuario = (id: number) => {
   return userRepo.getUsuarioById(id);
 };
 
-export const createUsuario = (data: UsuarioCreate) => {
-  return userRepo.createUsuario(data);
+export const createUsuario = async (data: UsuarioCreate) => {
+  const { email, password } = data;
+
+  if (!email) throw { status: 400, message: 'El email es obligatorio' };
+  if (!isValidEmail(email)) throw { status: 400, message: 'Email inválido' };
+  if (!password) throw { status: 400, message: 'La contraseña es obligatoria' };
+  if (password.length < 7) throw { status: 400, message: 'La contraseña es demasiado corta' };
+  if (!isStrongPassword(password)) throw { status: 400, message: 'La contraseña no cumple requisitos de seguridad' };
+
+  const existingUser = await userRepo.getUsuarioByEmail(email);
+  if (existingUser) throw { status: 409, message: 'El email ya está registrado' };
+
+  const saltRounds = 10;
+  const password_hash = await bcrypt.hash(password, saltRounds);
+
+  const newUserData = {
+    ...data,
+    password_hash,
+  };
+  delete (newUserData as any).password; 
+
+  const newUser = await userRepo.createUsuario(newUserData);
+
+  return newUser;
 };
 
 export const updateUsuario = (id: number, data: UsuarioUpdate) => {
@@ -19,4 +42,16 @@ export const updateUsuario = (id: number, data: UsuarioUpdate) => {
 
 export const deleteUsuario = (id: number) => {
   return userRepo.deleteUsuario(id);
+};
+
+
+
+const isValidEmail = (email: string): boolean => {
+  // Regex simple para validar email
+  return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
+};
+
+const isStrongPassword = (password: string): boolean => {
+  // Al menos 7 caracteres, una mayúscula y un número
+  return /^(?=.*[A-Z])(?=.*\d).{7,}$/.test(password);
 };
