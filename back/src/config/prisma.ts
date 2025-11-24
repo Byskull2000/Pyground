c43373db44a2b76af90d3006fda7a6a7a482a1b1
PHansLM <pablito.hans@hotmail.com>
2025-11-23 22:05:07 -0400

    config: configuracion prisma client

--- back/src/config/prisma.ts
+++ back/src/config/prisma.ts
@@ -1,4 +1,21 @@
 import { PrismaClient } from "../../generated/prisma";
 
-const prisma = new PrismaClient();
+const {
+  DB_USER = "",
+  DB_PASSWORD = "",
+  DB_HOST = "",
+  DB_PORT = "",
+  DB_NAME = "",
+} = process.env;
+
+const connectionString = `postgresql://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}?schema=public`;
+
+const prisma = new PrismaClient({
+  datasources: {
+    db: {
+      url: connectionString,
+    },
+  },
+});
+
 export default prisma;
